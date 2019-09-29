473593846997f0f782f6bd3b5ce96827d7d915df
JBobda <J.BobdaT@gmail.com>
2019-09-28 22:09:44 -0500

    add file close

--- trading_bot.py
+++ trading_bot.py
@@ -4,6 +4,7 @@ def main():
     # Reading API Keys from file
     f = open("api_keys.txt")
     keys = f.read().splitlines()
+    f.close()
 
     # Connecting to the API
     api = tradeapi.REST(keys[0], keys[1], api_version='v2')
