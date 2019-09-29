24886d1c9391fd85dd53b8786742965a292745b3
JBobda <J.BobdaT@gmail.com>
2019-09-28 22:27:51 -0500

    Updated the architecture of the bot

---
+++ main.py
@@ -0,0 +1,7 @@
+from trading_bot import TradingBot
+
+def main():
+    TradingBot()
+
+if __name__ == "__main__":
+    main()
--- trading_bot.py
+++ trading_bot.py
@@ -1,14 +1,20 @@
 import alpaca_trade_api as tradeapi
 
-def main():
-    # Reading API Keys from file
-    f = open("api_keys.txt")
-    keys = f.read().splitlines()
-    f.close()
+class TradingBot:
+    def __init__(self):
+        self.readKeys()
 
-    # Connecting to the API
-    api = tradeapi.REST(keys[0], keys[1], api_version='v2')
-    print(api.list_orders)
+        # Connecting to the API
+        self.alpaca_api = tradeapi.REST(self.API_KEY, self.API_SECRET, self.APCA_API_BASE_URL, api_version='v2')
+        account = self.alpaca_api.get_account()
+        print(account.status)
+        print(self.alpaca_api.list_orders())
 
-if __name__ == "__main__":
-    main()
+    def readKeys(self):
+        # Reading API Keys from file
+        f = open("api_keys.txt")
+        keys = f.read().splitlines()
+        f.close()
+        self.API_KEY = keys[0]
+        self.API_SECRET = keys[1]
+        self.APCA_API_BASE_URL = keys[2]
