a1429232c17a8e59a81d3282aed1e443dcab2221
JBobda <J.BobdaT@gmail.com>
2019-09-28 22:06:34 -0500

    basic listing of positions

--- trading_bot.py
+++ trading_bot.py
@@ -1,5 +1,13 @@
+import alpaca_trade_api as tradeapi
+
 def main():
-    print("Hello")
+    # Reading API Keys from file
+    f = open("api_keys.txt")
+    keys = f.read().splitlines()
+
+    # Connecting to the API
+    api = tradeapi.REST(keys[0], keys[1], api_version='v2')
+    print(api.list_orders)
 
 if __name__ == "__main__":
     main()
