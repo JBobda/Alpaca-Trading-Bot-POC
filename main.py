7d4ffe158aaa584a1d11be65f1135a20b9237d4d
JBobda <J.BobdaT@gmail.com>
2019-09-28 22:39:19 -0500

    Created a couple of sample functions that wrap around the client library, making it easy to develop a strategy from this point

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from trading_bot import TradingBot
 
 def main():
-    TradingBot()
+    bot = TradingBot()
+    bot.submit_order("FB", 1, "buy", "market", "day")
+    print(bot.list_orders())
+    print(bot.list_positions())
 
 if __name__ == "__main__":
     main()
--- trading_bot.py
+++ trading_bot.py
@@ -2,13 +2,12 @@ import alpaca_trade_api as tradeapi
 
 class TradingBot:
     def __init__(self):
+        # Read Keys
         self.readKeys()
 
         # Connecting to the API
         self.alpaca_api = tradeapi.REST(self.API_KEY, self.API_SECRET, self.APCA_API_BASE_URL, api_version='v2')
-        account = self.alpaca_api.get_account()
-        print(account.status)
-        print(self.alpaca_api.list_orders())
+        self.account = self.alpaca_api.get_account()
 
     def readKeys(self):
         # Reading API Keys from file
@@ -18,3 +17,12 @@ class TradingBot:
         self.API_KEY = keys[0]
         self.API_SECRET = keys[1]
         self.APCA_API_BASE_URL = keys[2]
+
+    def submit_order(self, symbol, quantity, side, type, time_in_force, limit_price=None, stop_price=None, client_order_id=None):
+        self.alpaca_api.submit_order(symbol, quantity, side, type, time_in_force, limit_price, stop_price, client_order_id)
+
+    def list_orders(self, status=None, limit=None, after=None, until=None, direction=None):
+        return self.alpaca_api.list_orders(status=None, limit=None, after=None, until=None, direction=None)
+
+    def list_positions(self):
+        return self.alpaca_api.list_positions()
